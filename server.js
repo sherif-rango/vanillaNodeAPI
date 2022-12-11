ca836db5df306f1c9089c23d9fbaff2d052b50fa
khaled-alwakel <khaledmofreh33@gmail.com>
2022-12-12 01:52:04 +0200

    simple modification

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-const http = require('http');
+const http = require("http");
 const {
   getProducts,
   getProduct,
   createProduct,
   updateProduct,
   deleteProduct,
-} = require('./controllers/productController');
+} = require("./controllers/productController");
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/api/products' && req.method === 'GET') {
+  if (req.url === "/api/products" && req.method === "GET") {
     getProducts(req, res);
-  } else if (req.url.match(/\/api\/products\/\w+/) && req.method === 'GET') {
-    const id = req.url.split('/')[3];
+  } else if (req.url.match(/\/api\/products\/\w+/) && req.method === "GET") {
+    const id = req.url.split("/")[3];
     getProduct(req, res, id);
-  } else if (req.url === '/api/products' && req.method === 'POST') {
+  } else if (req.url === "/api/products" && req.method === "POST") {
     createProduct(req, res);
-  } else if (req.url.match(/\/api\/products\/\w+/) && req.method === 'PUT') {
-    const id = req.url.split('/')[3];
+  } else if (req.url.match(/\/api\/products\/\w+/) && req.method === "PUT") {
+    const id = req.url.split("/")[3];
     updateProduct(req, res, id);
-  } else if (req.url.match(/\/api\/products\/\w+/) && req.method === 'DELETE') {
-    const id = req.url.split('/')[3];
+  } else if (req.url.match(/\/api\/products\/\w+/) && req.method === "DELETE") {
+    const id = req.url.split("/")[3];
     deleteProduct(req, res, id);
   } else {
-    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.writeHead(404, { "Content-Type": "application/json" });
     res.end(
       JSON.stringify({
-        message: 'Route Not Found: Please use the api/products endpoint',
+        message: "Route Not Found: Please use the api/products endpoint",
       })
     );
   }
